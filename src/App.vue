37cf8d8b3df411ed8b16195a41a495e5a500f308
Meteor <refined90926@gmail.com>
2022-02-21 15:50:44 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <asideMenu @triggerMarkerPopup="openPopup" ref="menu" />
+    <maskMap ref="map"/>
+
+    <lightbox />
+  </div>
+</template>
+
+<script>
+import{ mapActions } from 'vuex' 
+import asideMenu from './components/asideMenu.vue'
+import lightbox from './components/lightbox.vue';
+import maskMap from './components/maskMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    asideMenu,
+    lightbox,
+    maskMap,
+  },
+  methods: {
+    ...mapActions(['fetchLocations', 'fetchPharmacies']),
+    openPopup(id) {
+      this.$refs.map.triggerPopup(id);
+    },
+  },
+  mounted() {
+    this.fetchLocations();
+    this.fetchPharmacies();
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
